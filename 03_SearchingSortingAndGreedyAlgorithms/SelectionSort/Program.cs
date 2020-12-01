cf4d38638639f265e1ca12144b7388c11110395a
eto1302 <martinangelov1302@gmail.com>
2020-12-01 18:34:43 +0200

    This is the Lab for the Searching, Sorting and Greedy Algorithms lecture from the Softuni course Algorithms Fundamentals

---
+++ 03_SearchingSortingAndGreedyAlgorithms/SelectionSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Sort(numbers);
+        }
+
+        private static void Sort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; ++i)
+            {
+                int temp = numbers[i];
+                int[] tempSubArray = numbers.TakeLast(numbers.Length - i).ToArray();
+                int minIndex = Array.IndexOf(numbers.TakeLast(numbers.Length - i).ToArray(), numbers.TakeLast(numbers.Length - i).ToArray().Min());
+                numbers[i] = numbers[minIndex + i];
+                numbers[minIndex + i] = temp;
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 03_SearchingSortingAndGreedyAlgorithms/SetCover/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace SetCover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine().Split(new[] {", "}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToList();
+            List<int[]> sets = new List<int[]>();
+            var selectedSets = new List<int[]>();
+            int end = int.Parse(Console.ReadLine());
+            for (int i = 0; i < end ; i++)
+            {
+                  sets.Add(Console.ReadLine().Split(new []{", "}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            }
+
+            while (universe.Count > 0)
+            {
+                var currentSet = sets.OrderByDescending(s => s.Count(
+                    e => universe.Contains(e))).FirstOrDefault();
+                selectedSets.Add(currentSet);
+                sets.Remove(currentSet);
+                foreach (var a in currentSet)
+                {
+                    universe.Remove(a);
+                }
+            }
+
+            Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine($"{string.Join(", ", set)}");
+            }
+        }
+    }
+}
