713faa448d8fbac30dabcfbdac31036880bc8b87
eto1302 <martinangelov1302@gmail.com>
2020-12-19 20:44:20 +0200

    These are the Dynamic Programming exercises from the SoftUni course Algorithms Fundamentals

---
+++ 06_IntroductionToDynamicProgrammingExercises/SumWithLimitedCoins/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumWithLimitedAmountOfCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coins = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int targetSum = int.Parse(Console.ReadLine());
+            var sums = GetSums(coins);
+            Console.WriteLine(sums[targetSum]);
+        }
+
+        private static Dictionary<int, int> GetSums(int[] coins)
+        {
+            var sums = new Dictionary<int, int>();
+            foreach (var coin in coins)
+            {
+                foreach (var sum in sums.Keys.ToArray())
+                {
+                    if (!sums.ContainsKey(sum + coin))
+                    {
+                        sums.Add(sum + coin, 1);
+                    }
+                    else
+                    {
+                        sums[sum + coin]++;
+                    }
+
+                }
+
+                if (!sums.ContainsKey(coin))
+                {
+                    sums.Add(coin, 1);
+                }
+                else
+                {
+                    sums[coin]++;
+                }
+            }
+
+            return sums;
+        }
+    }
+}
